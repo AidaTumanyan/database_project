0bed16edf0574187298ccd6349e21a061cad8cca
AidaTumanyan <115363678+AidaTumanyan@users.noreply.github.com>
2023-12-24 00:35:20 +0400

    Add files via upload

--- models.py
+++ models.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 engine = create_engine('postgresql://aida:abcd@localhost:5432/database_project', echo=True)
 
 Base = declarative_base()
