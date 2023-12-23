43464bc15f1754e266b89d1a5fc73ab9fff4fab1
AidaTumanyan <115363678+AidaTumanyan@users.noreply.github.com>
2023-12-23 23:39:23 +0400

    Add files via upload

--- appy.py
+++ appy.py
@@ -1,32 +1,28 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from typing import List
 from pydantic import BaseModel
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
+# Create your FastAPI app
 app = FastAPI()
 
+# Define your SQLAlchemy engine and session
+engine = create_engine('postgresql://aida:abcd@localhost:5432/database_project')
+SessionLocal = sessionmaker(bind=engine)
+
 # Import your SQLAlchemy models
-from models import Transport, Route, Path  # Import your SQLAlchemy models here
+from models import Transport  # Add other models as needed
 
-# ... Define your FastAPI app, engine, and session as before ...
-
-# Define Pydantic models for request and response handling
-class Transport(BaseModel):
+# Pydantic models for request and response handling
+class TransportCreate(BaseModel):
     name: str
     speed: int
     num_cars: int
     fuel: str
 
-class Transport(BaseModel):
+class TransportResponse(BaseModel):
     id: int
     name: str
     speed: int
@@ -34,18 +30,26 @@ class Transport(BaseModel):
     fuel: str
 
 # CRUD operations for Transport
-@app.post("/transports/", response_model=Transport)
-def create_transport(transport: Transport, db: Session = Depends(get_db)):
-    # Implement logic to create a transport entry in the database
-    pass
+@app.post("/transports/", response_model=TransportResponse)
+def create_transport(transport: TransportCreate, db: Session = Depends(SessionLocal)):
+    try:
+        db_transport = Transport(**transport.dict())
+        db.add(db_transport)
+        db.commit()
+        db.refresh(db_transport)
+        return db_transport
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
 
-@app.get("/transports/{transport_id}", response_model=Transport)
-def read_transport(transport_id: int, db: Session = Depends(get_db)):
-    # Implement logic to retrieve a specific transport entry from the database
-    pass
+@app.get("/transports/{transport_id}", response_model=TransportResponse)
+def read_transport(transport_id: int, db: Session = Depends(SessionLocal)):
+    transport = db.query(Transport).filter(Transport.id == transport_id).first()
+    if transport is None:
+        raise HTTPException(status_code=404, detail="Transport not found")
+    return transport
 
-@app.get("/transports/", response_model=List[Transport])
-def list_transports(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
-    # Implement logic to retrieve a list of transport entries from the database
-    pass
+@app.get("/transports/", response_model=List[TransportResponse])
+def list_transports(skip: int = 0, limit: int = 10, db: Session = Depends(SessionLocal)):
+    transports = db.query(Transport).offset(skip).limit(limit).all()
+    return transports
 
